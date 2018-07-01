5745765c1c23b1e7f9b9cc3de031327175f7bc48
nitish24n <nitish24m@gmail.com>
2018-07-01 19:30:27 +0530

    Sending status codes and location headers - 1
    continued...

--- src/main/java/com/myCode/DemoJersey/resources/MessageResource.java
+++ src/main/java/com/myCode/DemoJersey/resources/MessageResource.java
@@ -1,6 +1,8 @@
 package com.myCode.DemoJersey.resources;
 
 
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.List;
 
 import javax.ws.rs.Consumes;
@@ -12,6 +14,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import com.myCode.DemoJersey.model.Employee;
 import com.myCode.DemoJersey.service.EmployeeService;
@@ -29,7 +33,7 @@ public class MessageResource {
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public List<Employee> getMessages() {
+	public List<Employee> getEmployees() {
 		return empService.getAllEmployees();
 	}
 	
@@ -43,9 +47,12 @@ public class MessageResource {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Employee addEmployee(Employee employee) {
-		
-		return empService.addEmployee(employee);
+	public Response addEmployee(Employee employee) throws URISyntaxException {
+		Employee newEmp = empService.addEmployee(employee);
+		return Response.created(new URI("/DemoJersey/webapi/employees/"+newEmp.getId()))
+						.entity(newEmp)
+						.build();
+		//return empService.addEmployee(employee);
 	}
 	
 	@PUT
