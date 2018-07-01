8fe7873be446bf209be025cf14ccdbbbfb6a1e52
nitish24n <nitish24m@gmail.com>
2018-07-01 19:50:23 +0530

    Sending status codes and location headers - 2
    end

--- src/main/java/com/myCode/DemoJersey/resources/MessageResource.java
+++ src/main/java/com/myCode/DemoJersey/resources/MessageResource.java
@@ -13,9 +13,10 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.core.UriInfo;
 
 import com.myCode.DemoJersey.model.Employee;
 import com.myCode.DemoJersey.service.EmployeeService;
@@ -47,9 +48,11 @@ public class MessageResource {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response addEmployee(Employee employee) throws URISyntaxException {
+	public Response addEmployee(Employee employee, @Context UriInfo uriInfo){
 		Employee newEmp = empService.addEmployee(employee);
-		return Response.created(new URI("/DemoJersey/webapi/employees/"+newEmp.getId()))
+		String newId = String.valueOf(newEmp.getId());
+		URI uri = uriInfo.getAbsolutePathBuilder().path(newId).build();
+		return Response.created(uri)
 						.entity(newEmp)
 						.build();
 		//return empService.addEmployee(employee);
