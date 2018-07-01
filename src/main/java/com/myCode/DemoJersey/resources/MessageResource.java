d3dde7bd8a137dc11484c39f8c998b873033c7f4
nitish24n <nitish24m@gmail.com>
2018-07-01 15:51:33 +0530

    Implementing POST method

--- src/main/java/com/myCode/DemoJersey/resources/MessageResource.java
+++ src/main/java/com/myCode/DemoJersey/resources/MessageResource.java
@@ -3,7 +3,9 @@ package com.myCode.DemoJersey.resources;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -34,5 +36,13 @@ public class MessageResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Employee test(@PathParam("id") int id) {
 		return empService.getEmployee(id);
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Employee addEmployee(Employee employee) {
+		
+		return empService.addEmployee(employee);
 	}
 }
