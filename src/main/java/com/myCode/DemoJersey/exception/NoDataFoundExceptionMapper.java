fc5d6e37188739363e025d02d529c2cf7f53a539
nitish24n <nitish24m@gmail.com>
2018-07-02 11:29:51 +0530

    Exception handling - 2
    Getting error message

--- src/main/java/com/myCode/DemoJersey/exception/NoDataFoundExceptionMapper.java
+++ src/main/java/com/myCode/DemoJersey/exception/NoDataFoundExceptionMapper.java
@@ -5,13 +5,17 @@ import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
 
+import com.myCode.DemoJersey.model.ErrorMessage;
+
 @Provider
 public class NoDataFoundExceptionMapper implements ExceptionMapper<NoDataFoundException>{
 
 	@Override
 	public Response toResponse(NoDataFoundException ex) {
-		// TODO Auto-generated method stub
-		return Response.status(Status.NOT_FOUND).build();
+		ErrorMessage errorMessage = new ErrorMessage(ex.getMessage(),"404", "https://www.google.co.in/search?q=404+error&oq=404+error&aqs=chrome..69i57.4526j0j7&sourceid=chrome&ie=UTF-8");
+		return Response.status(Status.NOT_FOUND)
+				.entity(errorMessage)
+				.build();
 	}
    
 }
